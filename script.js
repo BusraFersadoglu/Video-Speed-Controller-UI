ed9cac3786d950accd84811a706c99db9bdaa8a2
Büşra Ferşadoğlu <99492479+BusraFersadoglu@users.noreply.github.com>
2022-08-09 12:16:05 +0300

    Add functions

---
+++ script.js
@@ -0,0 +1,18 @@
+const video = document.querySelector(".video");
+const speed = document.querySelector(".speed");
+const speedBar = document.querySelector(".speed-bar");
+
+function handleMove(e){
+    const y = e.pageY - this.offsetTop;
+    const percent = y / this.offsetHeight;
+    const min = 0.4;
+    const max = 4;
+    const height = Math.round(percent * 100) + '%';
+    const playbackRate = percent * (max - min) + min;
+
+    speedBar.style.height = height;
+    speedBar.textContent = playbackRate.toFixed(2) + 'X';
+    video.playbackRate = playbackRate;
+}
+
+speed.addEventListener("mousemove", handleMove);
